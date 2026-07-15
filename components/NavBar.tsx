1e45ba854889ce5b71bb358fde0eb959414d8f58
rheaney-r7 <rhiannon_heaney@rapid7.com>
2026-07-15 11:12:55 +0100

    pushing updates and new tab

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,8 +2,9 @@ import * as React from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import styles from '../styles/NavBar.module.css';
+import buttonStyles from '../styles/Button.module.css';
 import { SetStateAction } from 'react';
-import Footer from './Footer';
+import Logo from '/public/images/logo.png';
 
 export interface NavBarProps {
     menu: boolean;
@@ -12,79 +13,89 @@ export interface NavBarProps {
 
 export default function NavBar({menu, setMenu}: NavBarProps)
 {
-
-    const [click, setClick] = React.useState(false);
     const router = useRouter();
 
-    const handleClick = () => setClick(!click);
-    const Close = () => setClick(false);
-
     return (
-        <div>
-            <div className={menu ? styles.mainContainer :  styles.mainContainerMobile} onClick={() => Close()} />
+        <div className={`${styles.navWrapper} ${menu ? styles.navWrapperOpen : ''}`}>
+            <div className={styles.mobileHeader}>
+                <Link href="./">
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                    <img className={styles.mobileLogo} src={Logo.src} alt="shine-logo" />
+                </Link>
+                <ul className={styles.mobileSocials}>
+                    <li><a href="https://www.tiktok.com/@shinebelfast" target="_blank" rel="noreferrer">
+                        <i className="fa-brands fa-tiktok"></i></a>
+                    </li>
+                    <li><a href="https://twitter.com/ShineBelfast" target="_blank" rel="noreferrer"><i className="fa-brands fa-twitter"></i></a></li>
+                    <li><a href="https://www.facebook.com/ShineBelfastNI/" target="_blank" rel="noreferrer"><i className="fa-brands fa-facebook"></i></a></li>
+                    <li><a href="https://www.instagram.com/shine.belfast/" target="_blank" rel="noreferrer"><i className="fa-brands fa-instagram"></i></a></li>
+                </ul>
+            </div>
             <nav className={styles.navbar} onClick={e => e.stopPropagation()}>
-             <div className={menu ? styles.navContainer :  styles.navContainerMobile}>
+                <div className={styles.navContainer}>
                     <ul className={styles.navMenu}>
-                        <li className={styles.navItem}  onClick={() => setMenu(false)}>
-                            <Link
-                                href="./"
-                                legacyBehavior
-                                className={router.pathname == "/" ? (styles.navLinks, styles.active) : styles.navLinks}
-                            >
-                                All Shows
-                            </Link>
-                        </li>
-                        <li className={styles.navItem}  onClick={() => setMenu(false)}>
-                            <Link
-                                href="./club-events"
-                                legacyBehavior
-                                className={router.pathname == "/" ? (styles.navLinks, styles.active) : styles.navLinks}
-
-                            >
-                                Club Events
+                        <li className={styles.navItem} onClick={() => setMenu(false)}>
+                            <Link href="./" legacyBehavior>
+                                <a className={router.pathname === "/" ? styles.active : styles.navLinks}>
+                                    All Shows
+                                </a>
                             </Link>
                         </li>
                         <li className={styles.navItem} onClick={() => setMenu(false)}>
-                            <Link
-                                legacyBehavior
-                                href="./comedy"
-                                className={router.pathname == "/" ? (styles.navLinks, styles.active) : styles.navLinks}
-                            >
-                                Comedy
+                            <Link href="./club-events" legacyBehavior>
+                                <a className={router.pathname === "/club-events" ? styles.active : styles.navLinks}>
+                                    Club Events
+                                </a>
                             </Link>
                         </li>
                         <li className={styles.navItem} onClick={() => setMenu(false)}>
-                            <Link
-                                legacyBehavior
-                                href="./live-gigs"
-                                className={styles.active}
-                            >
-                                Live Gigs
-                            </Link>
-                        </li>
-
-                        <li className={styles.navItem} onClick={() => setMenu(false)}>
-                            <Link
-                                legacyBehavior
-                                href="./help-and-FAQs"
-                                className={router.pathname == "/" ? (styles.navLinks, styles.active) : styles.navLinks}
-                            >
-                                {"Help / FAQ\'s"}
+                            <Link href="./comedy" legacyBehavior>
+                                <a className={router.pathname === "/comedy" ? styles.active : styles.navLinks}>
+                                    Comedy
+                                </a>
                             </Link>
                         </li>
                         <li className={styles.navItem} onClick={() => setMenu(false)}>
-                            <Link
-                                legacyBehavior
-                                href="https://shine.tickets"
-                                className={router.pathname == "/" ? (styles.navLinks, styles.active) : styles.navLinks}>
-                               {"BUY TICKETS"}
+                            <Link href="./live-gigs" legacyBehavior>
+                                <a className={router.pathname === "/live-gigs" ? styles.active : styles.navLinks}>
+                                    Live Gigs
+                                </a>
                             </Link>
                         </li>
-
+                        <li className={styles.navItem} onClick={() => setMenu(false)}>
+                            <Link href="./fleadh-2026" legacyBehavior>
+                                <a className={router.pathname === "/fleadh-2026" ? styles.active : styles.navLinks}>
+                                    Fleadh 2026
+                                </a>
+                            </Link>
+                        </li>
+                        <li className={styles.navItem} onClick={() => setMenu(false)}>
+                            <Link href="./help-and-FAQs" legacyBehavior>
+                                <a className={router.pathname === "/help-and-FAQs" ? styles.active : styles.navLinks}>
+                                    {"Help / FAQ's"}
+                                </a>
+                            </Link>
+                        </li>
+                        <li className={styles.navItem} onClick={() => setMenu(false)}>
+                            <Link href="https://shine.tickets" legacyBehavior>
+                                <a className={styles.navLinks}>
+                                    {"BUY TICKETS"}
+                                </a>
+                            </Link>
+                        </li>
                     </ul>
                 </div>
-            </nav >
-            {menu && <div className={styles.navFooter}><Footer /></div>}
-        </ div >
+            </nav>
+            <div className={styles.mailingSection}>
+                <form id="nav-subscribe-form" action="//www.venuecloud.net/s/f/27/17" method="POST" className={styles.mailingForm}>
+                    <input type="hidden" name="contact_permission" value="yes"/>
+                    <input className={styles.mailingInput} type="text" name="firstname" placeholder="First Name" required />
+                    <input className={styles.mailingInput} type="text" name="surname" placeholder="Surname" required />
+                    <input className={styles.mailingInput} type="text" name="email" placeholder="Email" required />
+                    <input type="hidden" id="source" name="source" value="website"/>
+                    <input className={buttonStyles.primary} value='Join Mailing List' type="submit" name="btnSubmit" />
+                </form>
+            </div>
+        </div>
     );
 }
---
+++ pages/fleadh-2026.tsx
@@ -0,0 +1,68 @@
+import styles from '../styles/Home.module.css'
+import NavBar from '../components/NavBar'
+import Breadcrumbs from '../components/Breadcrumbs'
+import Layout from '../components/Layout'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import { ResponseData } from './comedy'
+import React, { useState } from 'react'
+import Card from '../components/Card'
+import Button from '../components/Button'
+import Loading from '../components/Loading'
+
+export interface Fleadh2026Props {
+  menu: boolean;
+  setMenu: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function Fleadh2026({menu, setMenu}:Fleadh2026Props)
+{
+  const [data, setdata] = React.useState<ResponseData>()
+  const [isLoading, setLoading] = React.useState(false)
+
+  React.useEffect(() =>
+  {
+    setLoading(true)
+    fetch('https://www.venuecloud.net/api/events?venuecloudid=27&category=12')
+      .then((res) => res.json())
+      .then((data) =>
+      {
+        setdata(data)
+        setLoading(false)
+      })
+  }, [])
+
+  if (isLoading) return <Loading/>
+  if (!data) return <p>No Fleadh 2026 events</p>
+
+  const gigs = data?.events;
+
+  return (
+    <div className={styles.container}>
+      {!menu && <div className={styles.backMobile} onClick={()=> setMenu(true)}><i className="fa-solid fa-arrow-left"></i> </div>}
+      <Header route='Fleadh 2026' />
+      <NavBar menu={menu} setMenu={setMenu} />
+      <Breadcrumbs />
+      <main className={!menu ? styles.main : styles.mainMobile}>
+        <Layout title='Fleadh 2026' data={gigs}>
+          {(!isLoading && gigs instanceof Array) && gigs?.map((gig: any, index: number) => (
+            <Card
+              key={index}
+              gig={{
+                time: gig?.doors,
+                startDate: gig?.startDate,
+                name: gig?.title,
+                support: gig?.subTitle,
+                location: gig?.venue,
+                websiteImage: gig?.websiteImage,
+                ticketsUrl: gig?.ticketsUrl,
+                status: gig?.isSoldOut
+              }}
+            />
+          ))}
+        </Layout>
+      </main>
+      <Footer menu={menu}/>
+    </div>
+  )
+}
